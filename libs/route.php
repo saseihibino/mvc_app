07d05f3203564ed060af5b0d78e35c9756e69b54
saseihibino <132185275+saseihibino@users.noreply.github.com>
2023-10-05 18:45:03 +0900

    Initial commit

---
+++ libs/route.php
@@ -0,0 +1,64 @@
+<?php
+function route($path, $httpMethod){
+    try {
+        list($controller, $method) = explode('/', $path);
+        $case = [$method, $httpMethod];
+        switch ($controller) {
+            case 'home':
+                $controllerName = 'HomeController';
+                switch ($case) {
+                    case ['index', 'get']:
+                        $methodName = 'index';
+                        break;
+                    default:
+                        $controllerName = '';
+                        $methodName = '';
+                }
+                break;
+            case 'user':
+                $controllerName = 'UserController';
+                switch ($case) {
+                    case ['log-in', 'get']:
+                        $methodName = 'logIn';
+                        break;
+                    case ['sign-up', 'get']:
+                        $methodName ='signUp';
+                        break;
+                    case ['create', 'post']:
+                        $methodName = 'create';
+                        break;
+                    case ['log-out','get']:
+                        $methodName = 'logOut';
+                        break;
+                    case ['certification','post']:
+                        $methodName = 'certification';
+                        break;
+                    case ['my-page', 'get']:
+                        $methodName ='myPage';
+                        break;
+                    case ['edit','get']:
+                        $methodName ='edit';
+                        break;
+                    case ['update','post']:
+                        $methodName = 'update';
+                        break; 
+                    case ['delete','get']:
+                        $methodName = 'delete';
+                        break;
+                }
+            break;
+            default:
+                $controllerName = '';
+                $methodName = '';
+        }
+        require_once (ROOT_PATH."Controllers/{$controllerName}.php");
+
+        $obj = new $controllerName();
+        $obj->$methodName();
+
+    } catch (Throwable $e) {
+        error_log($e->getMessage());
+        header("HTTP/1.0 404 Not Found");
+        exit();
+    }
+}
